07a2bc62f4cf748771d8adf6f0c5afc5892c6c64
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-02 19:13:48 +0300

    Refactor Plus, Mul
    +    val: str = field(init=False, default="*")

--- node.py
+++ node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -23,7 +23,10 @@ class BinOp(Node):
 
 @dataclass
 class Plus(BinOp):
-    pass
+    val: str = field(init=False, default="+")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.left}, {self.right})"
 
 
 @dataclass
@@ -33,7 +36,10 @@ class Minus(BinOp):
 
 @dataclass
 class Mul(BinOp):
-    pass
+    val: str = field(init=False, default="*")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.left}, {self.right})"
 
 
 @dataclass
