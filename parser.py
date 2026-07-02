440dbcfc7d320c900055a61946497a70bd889f9e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-07-02 18:21:11 +0300

    Fix python/mypy error

--- parser.py
+++ parser.py
@@ -1,46 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Iterator, cast
 import node as N
 import iter_tokens as T
 
 
 class Parser:
     def __init__(self):
-        pass
+        self.token: T.Token = cast(T.Token, T.Tokens.WS)
+        self.tokens: Iterator[T.Token] = iter(())
 
     def _advance(self):
         try:
-            self.token: T.Token = next(self.tokens)
+            self.token = next(self.tokens)
         except StopIteration:
             self.token = None
         return self.token
 
-    def _expect(self, expected: T.Token):
+    def _expect(self, expected: T.Token) -> None:
         if self.token != expected:
             raise SyntaxError(f"Got {self.token}: expected {expected}")
         self._consume()
 
     def _consume(self) -> None:
         """Consume one token unconditionally"""
-        self.token: T.Token = next(self.tokens)
+        self.token = next(cast(Iterator[T.Token], self.tokens))
 
     def expr(self):
         res = self.term()
         op: T.Token
-        while (op := self.token) and (op in (T.Tokens.PlUS, T.Tokens.Minus)):
+        while (op := self.token) and (op in (T.Tokens.PLUS, T.Tokens.MINUS)):
             self._consume()
             right = self.term()
-            res = N.Plus(res, right) if op == T.Tokens.PlUS else N.Minus(res, right)
+            res = N.Plus(res, right) if op == T.Tokens.PLUS else N.Minus(res, right)
         return res
 
     def term(self):
         res = self.factor()
         op: T.Token()
-        while (op := self.token) and (op in ()):
+        while (op := self.token) and (op in (T.Tokens.MUL, T.Tokens.DIV)):
             self._consume()
             right = self.factor()
-            res = N.Mul(res, right) if op == T.Tokens.Mul else N.Div(res, right)
+            res = N.Mul(res, right) if op == T.Tokens.MUL else N.Div(res, right)
         return res
 
     def factor(self):
@@ -50,7 +52,7 @@ class Parser:
             res = self.expr()
             self._expect(T.Tokens.RPAREN)
         else:
-            res = N.Num(float(self.token))
+            res = N.Num(float(self.token.val))
             self._advance()
         return res
 
